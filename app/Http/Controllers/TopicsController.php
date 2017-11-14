e1d5974d1c9ecb2c25c7df76eb9e7cc4bf3d6773
vagrant <vagrant@homestead>
2017-11-14 08:33:44 +0000

    话题详情页

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -38,7 +38,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('message', '创建成功.');
     }
 
     public function edit(Topic $topic)
@@ -52,7 +52,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('message', '修改成功.');
     }
 
     public function destroy(Topic $topic)
@@ -60,7 +60,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+        return redirect()->route('topics.index')->with('message', '删除成功.');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
