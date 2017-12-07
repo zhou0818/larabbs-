1c65c49b1d639465048cf61532247e2f0e7218f5
vagrant <vagrant@homestead>
2017-12-07 11:31:53 +0000

    未读回复标记

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
@@ -16,6 +15,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     public function topics()
     {
@@ -30,5 +43,12 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -38,51 +38,61 @@
                 @guest
                     <li><a href="{{ route('login') }}">登录</a></li>
                     <li><a href="{{ route('register') }}">注册</a></li>
-                    @else
-                        <li>
-                            <a href="{{ route('topics.create') }}">
-                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
+                @else
+                    <li>
+                        <a href="{{ route('topics.create') }}">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                    {{-- 消息通知标记 --}}
+                    <li>
+                        <a href="{{ route('notifications.index') }}" class="notifications-badge"
+                           style="margin-top: -2px;">
+                            <span class="badge badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'fade' }} "
+                                  title="消息提醒">
+                                {{ Auth::user()->notification_count }}
+                            </span>
+                        </a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false">
                          <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                              <img src="{{ Auth::user()->avatar }}"
                                   class="img-responsive img-circle" style="width: 30px;height: 30px">
                          </span>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('users.show', Auth::id()) }}">
-                                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                                        个人中心
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('users.edit', Auth::id()) }}">
-                                        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
-                                        编辑资料
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    个人中心
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                    编辑资料
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                        <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
-                                        退出登录
-                                    </a>
+                                    <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+                                    退出登录
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                        @endguest
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
