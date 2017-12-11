56a4100d38dc6efa02459ab19d33c407edde4489
vagrant <vagrant@homestead>
2017-12-11 09:51:46 +0000

    增加边栏活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Auth;
 use App\Handlers\ImageUploadHandler;
@@ -16,10 +17,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic, User $user)
     {
         $topics = $topic->withOrder($request->order)->paginate(20);
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'active_users'));
     }
 
     public function show(Request $request, Topic $topic)
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasRoles;
+    use Traits\ActiveUserHelper;
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
