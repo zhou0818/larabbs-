8c76ab5c585b2cd2f94acc744a2a2284b200c404
zhou <95430589@qq.com>
2018-01-25 02:24:16 +0000

    user can register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,17 @@ class User extends Authenticatable
     use Traits\ActiveUserHelper;
     use Traits\LastActivedAtHelper;
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar'
+        'name',
+        'phone',
+        'email',
+        'password',
+        'introduction',
+        'avatar'
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     use Notifiable {
